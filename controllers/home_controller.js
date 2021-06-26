d9433c8bc4ac168371c048a9786efc84bede2f52
Ankit kumar yadav <kumar2001y@gmail.com>
2021-06-26 09:53:39 +0530

    starting flash messaging

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@ const post = require('../models/post');
 const User = require('../models/user');
 
 
-module.exports.home = function(req,res){
+// module.exports.home = function(req,res){
     // return res.end('<h1> express is up for codial</h1>');
     // res.cookie('user_id', 25);
     // console.log(req.cookies);
@@ -15,23 +15,51 @@ module.exports.home = function(req,res){
     //     });
         // papulating user who have posted data
 
-        post.find({})
-        .populate('user')
-        .populate({
-            path:'comments',
-            populate:{
-                path:'user'
-            }
-        })
-        .exec(function(err, posts){
+//         post.find({})
+//         .populate('user')
+//         .populate({
+//             path:'comments',
+//             populate:{
+//                 path:'user'
+//             }
+//         })
+//         .exec(function(err, posts){
 
-             User.find({},function(err,users){
-                return res.render('home', {
-                    title: "Codeial | Home",
-                    posts:  posts,
-                    all_users:users
+//              User.find({},function(err,users){
+//                 return res.render('home', {
+//                     title: "Codeial | Home",
+//                     posts:  posts,
+//                     all_users:users
 
-                });
-             })
-        });
+//                 });
+//              })
+//         });
+// }
+
+// async await
+
+module.exports.home = async function(req,res){
+
+    try{
+
+    let posts = await post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    });
+
+    let users = await User.find({});
+    return res.render('home', {
+        title: "Codeial | Home",
+        posts:  posts,
+        all_users:users
+
+    });
+}catch(err){
+    console.log('Error',err);
+    return;
+}
 }
