8ffcb1dd34accca3dbb2739c013317bc369edba1
Ankit kumar yadav <kumar2001y@gmail.com>
2021-07-03 00:09:35 +0530

    converting to Ajax: creating post-sending data

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -8,11 +8,12 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success','Post published successfully');
         return res.redirect('back');
 
     }catch(err){
-        console.log('Error',err);
-        return;
+        req.flash('error',err);
+        return res.redirect('back');
     }
 }
 
@@ -25,12 +26,15 @@ module.exports.destroy = async function(req, res){
         if (post.user == req.user.id){
             post.remove();
             await Comment.deleteMany({post: req.params.id});
+
+            req.flash('success','Post deleted successfully with their comments');
                 return res.redirect('back');  
         }else{
-            return res.redirect('back');
+            req.flash('error','you can not delet this post');
+                return res.redirect('back');  
         }
     }catch(err){
-        console.log('Error',err);
-        return;
+        req.flash('error','err');
+                return res.redirect('back');  
     }     
 }
